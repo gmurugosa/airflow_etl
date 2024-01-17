70fd1fc3cbf9333511e4c40a97ff2dbaeee7481e
gmurugosa <gmurugosa@gmail.com>
2024-01-17 18:58:26 -0300

    Entrega 3

--- dags/crypto_etl_dag.py
+++ dags/crypto_etl_dag.py
@@ -1,11 +1,13 @@
 from datetime import datetime, timedelta, timezone
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from sqlalchemy import create_engine
 import pandas as pd
 import requests
+from sqlalchemy import create_engine
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.base_hook import BaseHook
+from airflow.models import Variable
 
-# Definir la configuración del DAG
+# Define the configuration of the DAG
 default_args = {
     'owner': 'gabriel',
     'start_date': datetime(2024, 1, 1),
@@ -16,77 +18,124 @@ default_args = {
 dag = DAG(
     'crypto_data_dag',
     default_args=default_args,
-    description='Obtiene información diaria de criptomonedas y actualiza base de datos',
-    schedule_interval=timedelta(days=1),  # Puedes ajustar la frecuencia de ejecución,
+    description='Get daily cryptocurrency information and update the database',
+    schedule_interval=timedelta(days=1),
     catchup=False,
 )
 
-# Redshift credentials and connection details
-dbname = 'data-engineer-database'
-user = 'gabriel_muru_coderhouse'
-password = '0fzrz29Pb1'
-host = 'data-engineer-cluster.cyhh5bfevlmn.us-east-1.redshift.amazonaws.com'
-port = '5439'
+# Retrieve Redshift connection details using Airflow Hook
+db_hook = BaseHook.get_hook(conn_id='amazon_redshift')
+engine = create_engine(db_hook.get_uri(), execution_options={"autocommit": True})
 
-engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{dbname}", execution_options={"autocommit": True})
+# List of cryptocurrencies to obtain
+coins = Variable.get("coins", deserialize_json=True).get("list")
 
-# Lista de monedas que se van a obtener
-coins = ['BTC', 'ETH', 'USDT', 'XRP', 'SOL', 'USDC', 'ADA', 'DOGE', 'AVAX', 'LTC']
+def get_yesterday_date():
+    current_date_utc = datetime.now(timezone.utc)
+    yesterday_utc = current_date_utc - timedelta(days=1)
+    return yesterday_utc.year, yesterday_utc.month, yesterday_utc.day
 
-def obtener_informacion_diaria(coin, year, month, day):
+def drop_aux_table():
+    query = "DROP TABLE IF EXISTS cryptodata_aux;"
+    run_query(query)
+
+def run_query(query):
+    try:
+        with engine.connect() as connection:
+            connection.execute(query)
+    except Exception as e:
+        logging.error(f"Error executing the query: {e}")
+
+def download_crypto_info(**kwargs):
+    # Get yesterday's date
+    year, month, day = get_yesterday_date()
+
+    # Drop auxiliary table
+    drop_aux_table()
+
+    # Download information for each cryptocurrency
+    for coin in coins:
+        coin_data = obtain_daily_information(coin, year, month, day)
+        if coin_data:
+            df = convert_to_dataframe(coin_data, coin)
+            print(f"Downloading information for {coin} on {year}/{month}/{day}:")
+            # Store the DataFrame in the task instance
+            kwargs['ti'].xcom_push(key=f"dataframe_{coin}", value=df)
+
+def obtain_daily_information(coin, year, month, day):
     url = f'https://www.mercadobitcoin.net/api/{coin}/day-summary/{year}/{month}/{day}/'
-
     try:
         response = requests.get(url)
         if response.status_code == 200:
             return response.json()
         else:
-            print(f"Fallo al obtener información de la moneda {coin}. Status code: {response.status_code}")
+            logging.error(f"Failed to obtain information for {coin}. Status code: {response.status_code}")
             return None
     except requests.RequestException as e:
-        print(f"Excepción en Request: {e}")
+        logging.error(f"Request Exception: {e}")
         return None
 
-def correr_query(query):
-    try:
-        with engine.connect() as connection:
-            connection.execute(query)
-    except Exception as e:
-        print(f"Error ejecutando la consulta: {e}")
+def convert_to_dataframe(data, coin):
+    if data:
+        df = pd.DataFrame([data])
+        df['date'] = pd.to_datetime(df['date']).dt.date
+        df['volume'] = pd.to_numeric(df['volume'])
+        df['quantity'] = pd.to_numeric(df['quantity'])
+        df['coin'] = coin
+        return df
+    return pd.DataFrame()
 
-def download_crypto_info():
-    # Obtengo la fecha de ayer en UTC
-    current_date_utc = datetime.now(timezone.utc)
-    yesterday_utc = current_date_utc - timedelta(days=1)
-    year, month, day = yesterday_utc.year, yesterday_utc.month, yesterday_utc.day
-
-    # Borro tabla auxiliar
-    correr_query("DROP TABLE IF EXISTS cryptodata_aux;")
-
-    # Recorro la lista de monedas y descargo la información
+def load_data_to_redshift(**kwargs):
+    ti = kwargs['ti']
     for coin in coins:
-        coin_data = obtener_informacion_diaria(coin, year, month, day)
-        if coin_data:
-            df = pd.DataFrame([coin_data])
-            print(f"Descargando información para la moneda {coin} de la fecha {year}/{month}/{day}:")
-            # Convierto los tipos de datos
-            df['date'] = pd.to_datetime(df['date']).dt.date
-            df['volume'] = pd.to_numeric(df['volume'])
-            df['quantity'] = pd.to_numeric(df['quantity'])
-            df['coin'] = coin
+        df = ti.xcom_pull(task_ids='download_crypto_info', key=f"dataframe_{coin}")
+        if not df.empty:
             df.to_sql("cryptodata_aux", engine, if_exists='append', index=False)
-            print("Datos cargados en Redshift")
+            print(f"Data loaded into Redshift for {coin}")
 
-# Definir las tareas
-tarea_download_crypto_info = PythonOperator(
+def execute_merge_on_redshift():
+    # Ejecuto un comando SQL MERGE entre la tabla principal y la tabla auxiliar
+    merge_sql = """
+                MERGE INTO cryptodata
+                USING cryptodata_aux AS source
+                ON cryptodata.coin = source.coin AND cryptodata.date = source.date
+                WHEN MATCHED THEN
+                    UPDATE SET
+                        opening = source.opening,
+                        closing = source.closing,
+                        lowest = source.lowest,
+                        highest = source.highest,
+                        volume = source.volume,
+                        quantity = source.quantity,
+                        amount = source.amount,
+                        avg_price = source.avg_price
+                WHEN NOT MATCHED THEN
+                    INSERT (coin, date, opening, closing, lowest, highest, volume, quantity, amount, avg_price)
+                    VALUES (source.coin, source.date, source.opening, source.closing, source.lowest, source.highest,
+                            source.volume, source.quantity, source.amount, source.avg_price);             
+            """
+    run_query(merge_sql)        
+
+# Define tasks
+task_download_crypto_info = PythonOperator(
     task_id='download_crypto_info',
     python_callable=download_crypto_info,
     provide_context=True,
     dag=dag,
 )
 
-# Establecer el flujo de tareas
-tarea_download_crypto_info
+task_load_data_to_redshift = PythonOperator(
+    task_id='load_data_to_redshift',
+    python_callable=load_data_to_redshift,
+    provide_context=True,
+    dag=dag,
+)
 
-if __name__ == "__main__":
-    dag.cli()
+task_execute_merge_on_redshift = PythonOperator(
+    task_id='execute_merge_on_redshift',
+    python_callable=execute_merge_on_redshift,
+    dag=dag,
+)
+
+# Set the task flow
+task_download_crypto_info >> task_load_data_to_redshift >> task_execute_merge_on_redshift
